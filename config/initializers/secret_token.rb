65765c0a902eec32d604e5014b62696653d8db94
Manel <manuel.roma01@gmail.com>
2011-04-26 12:21:53 +0200

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutDevise::Application.config.secret_token = 'dee9e31dad0d30cab9e7d5b81b4a888a88f22b46a0122dff29ed907e94c4cf24ac8d8e9478ce46b28beab69ac74d1de56b101242f159c5428a9fef8d309eaafc'
