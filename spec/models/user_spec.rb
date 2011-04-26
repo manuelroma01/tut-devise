551ae680bb1cd4ecf5a3c92e269dc97c26a15bc7
Manel <manuel.roma01@gmail.com>
2011-04-26 17:16:54 +0200

    Tut completado

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe User do
+  #it { should have_fields(:name, :email) }
+  #it { should validates_presence_of :name }
+  #it { should validates_uniqueness_of(:name) }
+  #it { should validates_uniqueness_of(:email) }
+end
